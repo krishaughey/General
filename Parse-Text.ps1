05e8013011d06daefb198dc66429622926ad9753
Kris Haughey <37224839+krishaughey@users.noreply.github.com>
2021-11-04 16:34:06 -0500

    Freedom - 20211104

--- Parse-Text.ps1
+++ Parse-Text.ps1
@@ -1,5 +1,5 @@
-Select-String -Path "C:\Users\khaughey\OneDrive - Freedom Mortgage Corporation\Documents\WORKING\Local Group Membership\LocalGroupMembership_UAT_ERRORS.txt" -Pattern '\[(.*?)\]' | % { $_.Matches } | % { $_.Value }
+Select-String -Path "C:\Users\khaughey\OneDrive - Freedom Mortgage Corporation\Documents\WORKING\Certificates\Connection_Errors.txt" -Pattern '\[(.*?)\]' | % { $_.Matches } | % { $_.Value }
 
 
-Select-String -Path "C:\Users\khaughey\OneDrive - Freedom Mortgage Corporation\Documents\WORKING\Local Group Membership\LocalGroupMembership_DEV_ERRORS.txt" -Pattern 'PSComputerName' -SimpleMatch | select line
+Select-String -Path "C:\Users\khaughey\OneDrive - Freedom Mortgage Corporation\Documents\WORKING\Certificates\Connection_Errors.txt" -Pattern 'PSComputerName' -SimpleMatch | select line
 
