c13cb0079d985dae2c60ab770c8913020a5f8a11
Kris Haughey <37224839+krishaughey@users.noreply.github.com>
2023-08-04 12:26:41 -0700

    sanatized, added desc to rename script

--- FILE-List-RenameFiles.ps1
+++ FILE-List-RenameFiles.ps1
@@ -1,4 +1,10 @@
-$Photos = "/Users/kristopherhaughey/temp/ProfilePhotos/Photos/" #target folder
-$CSV = "/Users/kristopherhaughey/temp/ProfilePhotos/MappingFile.csv" #path to CSV
+$photoDir = "/Users/kristopherhaughey/temp/ProfilePhotos/Photos/" #target folder
+$CSV = "/Users/kristopherhaughey/temp/ProfilePhotos/Mapping01.csv" #path to CSV
 
-cd ($Photos); Import-Csv ($CSV)| foreach {Rename-Item -path $_.path -newname $_.filename}
+cd ($photoDir); Import-Csv ($CSV)| foreach {
+    Rename-Item -path $_.name01 -newname $_.positionID}
+
+<# EXAMPLE CSV
+name01,positionID
+/Users/kristopherhaughey/temp/ProfilePhotos/Photos/aperson@DOMAIN.com.jpg,CTS011517.jpg
+#>
