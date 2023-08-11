ce8f9ae25d9d50f115d1ad871ea1db4903831ff3
Kris Haughey <37224839+krishaughey@users.noreply.github.com>
2023-08-10 20:24:28 -0700

    Restaurant Chooser
    choose random restaurant from list

---
+++ TEXT-List-Get-Random.ps1
@@ -0,0 +1,7 @@
+﻿clear
+$list = Get-Content "$env:USERPROFILE\Documents\SCRIPTS\list.txt"
+$randomValue = Get-Random $list
+if ($randomValue -ge 7) {
+    Write-Host "CONGRATS! You're getting -- $randomValue --" -ForegroundColor Cyan
+}
+pause
