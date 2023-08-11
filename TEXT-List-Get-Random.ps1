24f5cf463b4fadbe6c68ad9b239b447a8c46e0ee
Kris Haughey <37224839+krishaughey@users.noreply.github.com>
2023-08-10 20:36:36 -0700

    Update TEXT-List-Get-Random.ps1
    removed orphaned code from donor script

--- TEXT-List-Get-Random.ps1
+++ TEXT-List-Get-Random.ps1
@@ -1,7 +1,7 @@
 ﻿clear
 $list = Get-Content ".\list.txt"
 $randomValue = Get-Random $list
-if ($randomValue -ge 7) {
+{
     Write-Host "CONGRATS! You're getting -- $randomValue --" -ForegroundColor Cyan
 }
 pause
